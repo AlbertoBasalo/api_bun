13b79d27b13d9abe3aef76559ced49ef394dab51
Alberto Basalo <albertobasalo@hotmail.com>
2024-06-20 18:02:36 +0200

    feat: no default delay

--- src/api_bun.config.ts
+++ src/api_bun.config.ts
@@ -3,28 +3,28 @@ export type StorageTypes = "memory" | "file";
 export type SecurityTypes = "none" | "write";
 
 export type ApiBunConfig = {
-  /** Log level (info,none,verbose) */
-  LOG_LEVEL: LogLevels;
-  /** Storage type (memory,file) */
-  STORAGE: StorageTypes;
-  /** Security type (none,write) */
-  SECURITY: SecurityTypes;
-  /** Secret */
-  SECRET: string;
-  /** API root (api by default) */
-  API_ROOT?: string;
-  /** Forced timeout in milliseconds */
-  API_FORCED_TIMEOUT: number;
+	/** Log level (info,none,verbose) */
+	LOG_LEVEL: LogLevels;
+	/** Storage type (memory,file) */
+	STORAGE: StorageTypes;
+	/** Security type (none,write) */
+	SECURITY: SecurityTypes;
+	/** Secret */
+	SECRET: string;
+	/** API root (api by default) */
+	API_ROOT?: string;
+	/** Forced timeout in milliseconds */
+	API_FORCED_TIMEOUT: number;
 };
 
 /**
  * API Bun Configuration
  */
 export const API_BUN_CONFIG: ApiBunConfig = {
-  LOG_LEVEL: (Bun.env.LOG_LEVEL as LogLevels) || "info",
-  STORAGE: (Bun.env.STORAGE as StorageTypes) || "memory",
-  SECURITY: (Bun.env.SECURITY as SecurityTypes) || "none",
-  SECRET: (Bun.env.SECRET as string) || "secret",
-  API_ROOT: Bun.env.API_ROOT as string || "api",
-  API_FORCED_TIMEOUT: Number.parseInt(Bun.env.API_FORCED_TIMEOUT || '1000')
+	LOG_LEVEL: (Bun.env.LOG_LEVEL as LogLevels) || "info",
+	STORAGE: (Bun.env.STORAGE as StorageTypes) || "memory",
+	SECURITY: (Bun.env.SECURITY as SecurityTypes) || "none",
+	SECRET: (Bun.env.SECRET as string) || "secret",
+	API_ROOT: (Bun.env.API_ROOT as string) || "api",
+	API_FORCED_TIMEOUT: Number.parseInt(Bun.env.API_FORCED_TIMEOUT || "000"),
 };
